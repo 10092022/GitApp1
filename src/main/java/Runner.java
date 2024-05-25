b843990d27edf3b537a4765c9db538fc4765cbf4
Ali Qayıkcı <battal.aloska@gmail.com>
2024-05-25 19:21:45 +0300

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,10 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        // versiyon 1
+        System.out.println("hello GIT");
+
+        System.out.println("git add . : satagin areaya alır.");
+
+    }
+}
