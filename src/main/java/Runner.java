23061fd4f01723fa2307f22c0f03c782dac6a8a4
Ali Qayıkcı <battal.aloska@gmail.com>
2024-05-25 21:16:47 +0300

    v2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,7 +4,9 @@ public class Runner {
         // versiyon 1
         System.out.println("hello GIT");
 
-        System.out.println("git add . : satagin areaya alır.");
+        System.out.println("git add . : staging areaya alır.");
 
+        // version 2
+        System.out.println("git commit -m\"\" ile version olusturulur");
     }
 }
